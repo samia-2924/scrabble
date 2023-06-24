f6805b70d9686a71fdbc929ad431b1b8f2256ead
samia-2924 <axiepool0312@gmail.com>
2023-06-24 16:45:02 +0500

    project completion

--- src/main/java/com/example/scrabblegui/Tile.java
+++ src/main/java/com/example/scrabblegui/Tile.java
@@ -1,25 +1,41 @@
+/**
+ * The Tile class represents a Scrabble tile with a letter and its corresponding points.
+ * It implements the Serializable interface to allow the object to be serialized and deserialized.
+ */
 package com.example.scrabblegui;
 
 import java.io.Serializable;
 
 public class Tile implements Serializable {
-    private String letter;
-    private int points;
+    private String letter; // The letter on the tile
+    private int points; // The points associated with the letter
 
-
-    public Tile(String letter, int points)
-    {
+    /**
+     * Constructs a Tile object with the given letter and points.
+     *
+     * @param letter The letter on the tile.
+     * @param points The points associated with the letter.
+     */
+    public Tile(String letter, int points) {
         this.letter = letter;
         this.points = points;
     }
 
-    public String getLetter()
-    {
+    /**
+     * Retrieves the letter on the tile.
+     *
+     * @return The letter on the tile.
+     */
+    public String getLetter() {
         return letter;
     }
 
-    public int getPoints()
-    {
+    /**
+     * Retrieves the points associated with the letter on the tile.
+     *
+     * @return The points associated with the letter on the tile.
+     */
+    public int getPoints() {
         return points;
     }
 }
